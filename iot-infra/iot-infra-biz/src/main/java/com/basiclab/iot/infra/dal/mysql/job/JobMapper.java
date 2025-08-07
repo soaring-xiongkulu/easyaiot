88a3f0c273763e59817b7b9c5f8c3f3478246968
LF <123@163.com>
2025-08-07 10:51:29 +0800

    init：初始化项目

---
+++ iot-infra/iot-infra-biz/src/main/java/com/basiclab/iot/infra/dal/mysql/job/JobMapper.java
@@ -0,0 +1,30 @@
+package com.basiclab.iot.infra.dal.mysql.job;
+
+import com.basiclab.iot.common.domain.PageResult;
+import com.basiclab.iot.common.mybatis.core.mapper.BaseMapperX;
+import com.basiclab.iot.common.mybatis.core.query.LambdaQueryWrapperX;
+import com.basiclab.iot.infra.controller.admin.job.vo.job.JobPageReqVO;
+import com.basiclab.iot.infra.dal.dataobject.job.JobDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 定时任务 Mapper
+ *
+ * @author 安徽上洲智能科技
+ */
+@Mapper
+public interface JobMapper extends BaseMapperX<JobDO> {
+
+    default JobDO selectByHandlerName(String handlerName) {
+        return selectOne(JobDO::getHandlerName, handlerName);
+    }
+
+    default PageResult<JobDO> selectPage(JobPageReqVO reqVO) {
+        return selectPage(reqVO, new LambdaQueryWrapperX<JobDO>()
+                .likeIfPresent(JobDO::getName, reqVO.getName())
+                .eqIfPresent(JobDO::getStatus, reqVO.getStatus())
+                .likeIfPresent(JobDO::getHandlerName, reqVO.getHandlerName())
+        );
+    }
+
+}
---
+++ iot-infra/iot-infra-biz/src/main/java/com/basiclab/iot/infra/framework/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 属于 infra 模块的 framework 封装
+ *
+ * @author 安徽上洲智能科技
+ */
+package com.basiclab.iot.infra.framework;
