8f13ec28e54bbd1639b5eee2c6f1532d6e81180f
LF <123@163.com>
2025-08-07 10:42:32 +0800

    init：初始化项目

---
+++ iot-common/iot-common-base/src/main/java/com/basiclab/iot/common/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.basiclab.iot.common.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.TimeZone;
+
+/**
+ * 系统配置
+ *
+ * @author 安徽上洲智能科技
+ */
+public class ApplicationConfig
+{
+    /**
+     * 时区配置
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonObjectMapperCustomization()
+    {
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.timeZone(TimeZone.getDefault());
+    }
+}
