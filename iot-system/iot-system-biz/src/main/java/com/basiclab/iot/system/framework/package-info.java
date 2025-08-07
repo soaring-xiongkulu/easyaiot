2a87005285e2fed4ae331fc04367a587699b945d
LF <123@163.com>
2025-08-07 11:04:39 +0800

    init：初始化项目

---
+++ iot-system/iot-system-biz/src/main/java/com/basiclab/iot/system/framework/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 属于 system 模块的 framework 封装
+ *
+ * @author 安徽上洲智能科技
+ */
+package com.basiclab.iot.system.framework;
---
+++ iot-things/iot-things-biz/src/main/java/com/basiclab/iot/things/PendingOperation.java
@@ -0,0 +1,8 @@
+
+package com.basiclab.iot.things;
+
+public interface PendingOperation {
+
+    boolean isCanceled();
+
+}
