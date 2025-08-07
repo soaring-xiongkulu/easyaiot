a18db1b5493d6d639d1e66dfa5df4c9f6cf9676d
LF <123@163.com>
2025-08-07 10:43:44 +0800

    init：初始化项目

---
+++ iot-common/iot-common-test/src/main/java/com/basiclab/iot/common/core/ut/BaseMockitoUnitTest.java
@@ -0,0 +1,13 @@
+package com.basiclab.iot.common.core.ut;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * 纯 Mockito 的单元测试
+ *
+ * @author 安徽上洲智能科技
+ */
+@ExtendWith(MockitoExtension.class)
+public class BaseMockitoUnitTest {
+}
---
+++ iot-gateway/src/main/java/com/basiclab/iot/gateway/util/BannerApplicationRunner.java
@@ -0,0 +1,36 @@
+package com.basiclab.iot.gateway.util;
+
+import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 项目启动成功后，提供文档相关的地址
+ *
+ * @author 安徽上洲智能科技
+ */
+@Component
+@Slf4j
+public class BannerApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) {
+        ThreadUtil.execute(() -> {
+            ThreadUtil.sleep(1, TimeUnit.SECONDS); // 延迟 1 秒，保证输出到结尾
+            log.info("\n----------------------------------------------------------\n\t" +
+                            "项目启动成功！\n\t" +
+                            "接口文档: \t{} \n\t" +
+                            "开发文档: \t{} \n\t" +
+                            "视频教程: \t{} \n" +
+                            "----------------------------------------------------------",
+                    "https://gitee.com/DEEP-VISION-MACHINE/LF-AI-STREAM",
+                    "https://gitee.com/DEEP-VISION-MACHINE/LF-AI-STREAM",
+                    "https://gitee.com/DEEP-VISION-MACHINE/LF-AI-STREAM");
+        });
+    }
+
+}
