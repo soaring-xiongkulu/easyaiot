390242d40ad9d0485df8e1aed895e6869c41cc3a
翱翔的雄库鲁 <123@163.com>
2025-08-18 13:26:39 +0800

    udp(WEB)：修复前端PAGE_NOT_FOUND问题导致的页面加载失败

--- WEB/src/router/routes/basic.ts
+++ WEB/src/router/routes/basic.ts
@@ -1,10 +1,16 @@
 import type { AppRouteRecordRaw } from '@/router/types'
-import { EXCEPTION_COMPONENT, LAYOUT, PAGE_NOT_FOUND_NAME, REDIRECT_NAME } from '@/router/constant'
+import {
+  EXCEPTION_COMPONENT,
+  LAYOUT,
+  PAGE_NOT_FOUND_NAME,
+  PARENT_LAYOUT_NAME,
+  REDIRECT_NAME
+} from '@/router/constant'
 
 // 404 on a page
 export const PAGE_NOT_FOUND_ROUTE: AppRouteRecordRaw = {
   path: '/:path(.*)*',
-  name: PAGE_NOT_FOUND_NAME,
+  name: PARENT_LAYOUT_NAME,
   component: LAYOUT,
   meta: {
     title: 'ErrorPage',
@@ -36,7 +42,7 @@ export const REDIRECT_ROUTE: AppRouteRecordRaw = {
   },
   children: [
     {
-      path:'/redirect/:path(.*)',
+      path:'/redirect/:path(.*)/:_redirect_type(.*)/:_origin_params(.*)?',
       name: REDIRECT_NAME,
       component: () => import('@/views/base/redirect/index.vue'),
       meta: {
