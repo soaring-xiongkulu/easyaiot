e81c99260441566a715c6166d3e84cefd1d15577
翱翔的雄库鲁 <123@163.com>
2025-08-31 03:19:12 +0800

    udp(VIDEO)：修改字段错误

--- VIDEO/models.py
+++ VIDEO/models.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Device(db.Model):
-    id = db.Column(db.String(100), primary_key=True, nullable=False)
+    id = db.Column(db.String(100), primary_key=True)
     name = db.Column(db.String(100), nullable=True)
     source = db.Column(db.Text, nullable=False)
     rtmp_stream = db.Column(db.Text, nullable=False)
