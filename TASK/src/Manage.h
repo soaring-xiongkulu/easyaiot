576e33fa4a363a2da9834c1e4d7b8cf56116c89d
翱翔的雄库鲁 <123@163.com>
2025-10-15 18:39:15 +0800

    udp(TASK)：优化TASK整体逻辑

--- TASK/src/Manage.h
+++ TASK/src/Manage.h
@@ -12,40 +12,38 @@
 
 #include "config.h"
 
-namespace CManage {
-    std::atomic<int> s_exit(0);
+std::atomic<int> s_exit(0);
 
-    void procSignal(int s) {
-        LOG(INFO) << "receive signal: " << s << ",will exit...";
-        s_exit.store(1, std::memory_order_release);
-    }
-
-    void installSignalCallback() {
-        struct sigaction sigIntHandler;
-        sigIntHandler.sa_flags = 0;
-        sigIntHandler.sa_handler = procSignal;
-        sigemptyset(&sigIntHandler.sa_mask);
-        sigaction(SIGINT, &sigIntHandler, nullptr);
-        sigaction(SIGQUIT, &sigIntHandler, nullptr);
-        sigaction(SIGTERM, &sigIntHandler, nullptr);
-        sigaction(SIGPIPE, &sigIntHandler, nullptr);
-    }
-
-    class Server {
-    public:
-        explicit Server(const Config &conf);
-        ~Server();
-        void waitForShutdown();
-        bool start();
-        void stop();
-        bool isRun() const;
-        bool isTerminal() const;
-
-    private:
-        std::atomic<bool> _isRun{false};
-        std::atomic<bool> _isTerminal{false};
-        Config _local;
-        std::unique_ptr<videoDetect> _pVideoDetectHandle;
-    };
+void procSignal(int s) {
+    LOG(INFO) << "receive signal: " << s << ",will exit...";
+    s_exit.store(1, std::memory_order_release);
 }
+
+void installSignalCallback() {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_flags = 0;
+    sigIntHandler.sa_handler = procSignal;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigaction(SIGINT, &sigIntHandler, nullptr);
+    sigaction(SIGQUIT, &sigIntHandler, nullptr);
+    sigaction(SIGTERM, &sigIntHandler, nullptr);
+    sigaction(SIGPIPE, &sigIntHandler, nullptr);
+}
+
+class Server {
+public:
+    explicit Server(const Config &conf);
+    ~Server();
+    void waitForShutdown();
+    bool start();
+    void stop();
+    bool isRun() const;
+    bool isTerminal() const;
+
+private:
+    std::atomic<bool> _isRun{false};
+    std::atomic<bool> _isTerminal{false};
+    Config _local;
+    std::unique_ptr<videoDetect> _pVideoDetectHandle;
+};
 #endif  // MANAGE_H_
