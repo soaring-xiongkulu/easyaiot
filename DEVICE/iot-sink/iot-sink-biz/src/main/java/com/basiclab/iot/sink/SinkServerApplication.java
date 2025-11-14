4b0f7c2d96165a6900d46edaa029dc9f86a8de5a
翱翔的雄库鲁 <andywebjava@163.com>
2025-11-14 21:31:18 +0800

    udp(DEVICE)：增加物联网数据采集模块

--- DEVICE/iot-sink/iot-sink-biz/src/main/java/com/basiclab/iot/sink/SinkServerApplication.java
+++ DEVICE/iot-sink/iot-sink-biz/src/main/java/com/basiclab/iot/sink/SinkServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IotSinkApplication {
+public class SinkServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IotSinkApplication.class, args);
+        SpringApplication.run(SinkServerApplication.class, args);
     }
 
 }
